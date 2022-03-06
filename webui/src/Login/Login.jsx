0b29f6ba459b6a0586246a7583c51b23467dbf9d
AustinHeard <70654710+AustinHeard@users.noreply.github.com>
2022-03-06 00:39:34 -0500

    refactored for readability and rewrote handleSubmit

--- webui/src/Login/Login.jsx
+++ webui/src/Login/Login.jsx
@@ -10,6 +10,8 @@ const Login = () => {
   // temp info, will replace with link to actual database
   const database = [
     { username: "test",
+      password: "1234" },
+    { username: "test",
       password: "pwtest" },
     { username: "test2",
       password: "pwtest2" }
@@ -24,23 +26,50 @@ const Login = () => {
     //Prevent page reload
     event.preventDefault();
 
-    var { usernameError, passwordError } = document.forms[0];
+    var { username, password } = document.forms[0];
 
     // Find user login info
-    const userData = database.find((user) => user.username === usernameError.value);
+    /**
+     * TODO: this is broken when usernames are the same.
+     * 
+     *    This works
+     *    username: test
+     *    password: 1234
+     * 
+     *    But this doesn't work
+     *    username: test
+     *    password: pwtest
+     * 
+     *  the test that is working is in the database array first
+     * 
+     *  this is because of the databaseTest var but the if 
+     *  statements are written to only work with unique usernames.
+     * 
+     *  I want to be able to have duplicate usernames
+     * 
+     *  probably doesn't need to get fixed because
+     *  it will have to get changed when we introduce the database
+     */
 
-    // Compare user info
-    if (userData) {
-      if (userData.password !== passwordError.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.passwordError });
+    var databaseTest = database.map(x => (x.username === username.value) ? true : false)
+
+    // Checks if username is in database
+    if (databaseTest.includes(true)) {
+
+      // This also only works if there are unique usernames
+      var usernameIndex = databaseTest.indexOf(true)
+
+      // Checks if password is in database
+      if (database[usernameIndex]["password"] !== password.value) {
+        setErrorMessages({ name: "passwordError", message: errors.passwordError });
       } else {
         setIsSubmitted(true);
       }
+
     } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.usernameError });
+      setErrorMessages({ name: "usernameError", message: errors.usernameError });
     }
+    
   };
 
   // Generate code error message
@@ -55,12 +84,12 @@ const Login = () => {
       <form onSubmit={handleSubmit}>
         <div className="input-container">
           <label>Username </label>
-          <input type="text" name="usernameError" required />
+          <input type="text" name="username" required />
           {renderErrorMessage("usernameError")}
         </div>
         <div className="input-container">
           <label>Password </label>
-          <input type="password" name="passwordError" required />
+          <input type="password" name="password" required />
           {renderErrorMessage("passwordError")}
         </div>
         <div className="sign-up-button">
