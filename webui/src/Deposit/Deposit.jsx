c5dc89a2b475189b66b83bb445d1604a936474a5
AustinHeard <70654710+AustinHeard@users.noreply.github.com>
2022-03-10 15:37:38 -0500

    back button added

--- webui/src/Deposit/Deposit.jsx
+++ webui/src/Deposit/Deposit.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Deposit.css";
 
 export default class Deposit extends React.Component {
@@ -50,6 +51,12 @@ export default class Deposit extends React.Component {
                 value={this.state.depositAmount}
                 onChange={this.handleChange}
               />
+            </div>
+
+            <div className="sign-up-button">
+              <Link to={"/Account"}>
+                Account Overview  
+              </Link>
             </div>
 
             <div className = "buttons">
