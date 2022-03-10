053f42da2ab9808e9f339ff9119713a6145bb0c1
AustinHeard <70654710+AustinHeard@users.noreply.github.com>
2022-03-10 15:13:56 -0500

    account changed to ES6

--- webui/src/AccountSummary/Account.jsx
+++ webui/src/AccountSummary/Account.jsx
@@ -1,55 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./Account.css";
-const Account = () =>{
-  //TODO Take information from database and add to page
-  const depositClick =(onClick) =>{};
-  const withdrawClick =(onClick) =>{};
-  const transferClick =(onClick) =>{};
+export default class Account extends React.Component {
+  // States
+  constructor(props) {
+    super(props)
+    this.state = {
+      savingsBalance: '$20',
+      checkingBalance: '$20',
+      username: 'username'
+    };
+  }
+
   
+  //TODO: Take information from database and add to page
+  /**
+   * savingsBalance
+   * checkingBalance
+   * username
+   */
+  
+  render() {
+    return (
+      <div className="app">
+        <div className="login-form">
+          
+          <div className = "title">Hello, {this.state.username}</div>
+          <div className = "Balances">
 
-  return (
-    <div className="app">
-      <div className="login-form">
-        
-        <div className = "title">Hello, </div>
-        <div className = "Balances"> Your current balances are: 
+            Your current balances are: 
 
+            <div className = "acc-title">Savings: </div>
 
-          <div className = "acc-title"> Savings: </div>
-          [//savings from database]
-          <div className = "saving-balance">
-            <input readOnly value={"$20"} />
-          </div>
-        
+            <div className = "saving-balance">
+              <input readOnly value={this.state.savingsBalance} />
+            </div>
+          
+            <div className = "acc-title">Checking: </div>
 
-
-          <div className = "acc-title">Checking: </div>
-            [//checkings from database]
             <div className = "checking-balance">
-            <input readOnly value={"$20"} />
+              <input readOnly value={this.state.checkingBalance} />
+            </div>
+              
           </div>
-            
-        </div>
-        <div classNames = "buttons">
-          <Link to ="/Deposit"><button>
-            <label>Deposit</label>  
-          </button>
-          </Link>
 
-          <Link to ="/Withdraw"><button>
-            <label>Withdraw</label>  
-          </button>
-          </Link>
+          <div classNames = "buttons">
+            <Link to ="/Deposit">
+              <button>
+                <label>Deposit</label>  
+              </button>
+            </Link>
 
-          <Link to ="/Transfer"><button>
-            <label>Transfer</label>  
-          </button>
-          </Link>
+            <Link to ="/Withdraw">
+              <button>
+                <label>Withdraw</label>  
+              </button>
+            </Link>
+
+            <Link to ="/Transfer">
+              <button>
+                <label>Transfer</label>  
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
-
-export default Account;
