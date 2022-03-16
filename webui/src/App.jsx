c2957cf0cb48800d47cf0903906a8a129163622f
AustinHeard <70654710+AustinHeard@users.noreply.github.com>
2022-03-16 14:16:31 -0400

    Router moved to index.js

--- webui/src/App.jsx
+++ webui/src/App.jsx
@@ -1,6 +1,5 @@
-
+import React, { useState } from 'react';
 import {
-  BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
@@ -14,18 +13,21 @@ import Transfer from "./Transfer/Transfer";
 
 function App() {
 
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
 
   return (
-    <Router>
-        <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/SignUp" element={<SignUp/>}/>
-          <Route path="/Account" element={<Account/>}/>
-          <Route path="/Deposit" element={<Deposit/>}/>
-          <Route path="/Withdraw" element={<Withdraw/>}/>
-          <Route path="/Transfer" element={<Transfer/>}/>
-        </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/SignUp" element={<SignUp/>}/>
+      <Route path="/Account" element={<Account/>}/>
+      <Route path="/Deposit" element={<Deposit/>}/>
+      <Route path="/Withdraw" element={<Withdraw/>}/>
+      <Route path="/Transfer" element={<Transfer/>}/>
+    </Routes>
   );
 }
 
