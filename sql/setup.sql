b7c0a4dfa96d25c7544b9cd233d22b556d4bcdcc
AustinHeard <70654710+AustinHeard@users.noreply.github.com>
2022-03-23 20:13:53 -0400

    CreateUser procedure created

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,16 +1,42 @@
 CREATE DATABASE bankingApp
 GO
 
+
 USE bankingApp
 GO
+
 
 CREATE TABLE accountInformation (
 	accountNumber int NOT NULL PRIMARY KEY,
 	username VARCHAR(255),
-	passowrd VARCHAR(255),
+	password VARCHAR(255),
 	checkingBalance int,
 	savingsBalance int
 )
 GO
 
+
 INSERT INTO accountInformation VALUES (0, 'test', 'password', 100, 200)
+GO
+
+
+CREATE PROCEDURE CreateUser
+	(
+		@accountNumber int,
+		@username VARCHAR(255),
+		@password VARCHAR(255),
+		@checkingBalance int,
+		@savingsBalance int
+	)
+AS
+BEGIN
+	INSERT INTO accountInformation
+	VALUES(
+				@accountNumber,
+				@username,
+				@password,
+				@checkingBalance,
+				@savingsBalance
+			)
+END
+GO
