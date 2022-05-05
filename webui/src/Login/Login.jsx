00fc0e729ef60bda03d737f6e9856b6bc71aa628
AustinHeard <70654710+AustinHeard@users.noreply.github.com>
2022-05-05 13:08:00 -0400

    maybe

--- webui/src/Login/Login.jsx
+++ webui/src/Login/Login.jsx
@@ -19,7 +19,7 @@ export default class Login extends React.Component {
   }
 
   async loginUser(credentials) {
-    return fetch('http://localhost:3001/api/login', {
+    return fetch('http://localhost:8081/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
